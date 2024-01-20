daf00698bfea1ddc6286f6c090b427bc27526353
Tejas150 <tejaskamble1629@gmail.com>
2024-01-20 14:33:12 +0530

    Add route file for clean code.

---
+++ nodeServer/route.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    let filePath = 'message.txt';
+        fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error(`Error reading file: ${err.message}`);
+            return;
+        }
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>'); // Fix: Correct head tag
+        res.write(
+            `<body><p>${data}</p><form action="/userName" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`
+        );
+        res.write('</html>');
+        return res.end();
+        });
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = `${parsedBody.split('=')[1]}<br>`;
+      fs.appendFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+  }
+//   res.setHeader('Content-Type', 'text/html');
+//   res.write('<html>');
+//   res.write('<head><title>My First Page</title><head>');
+//   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+//   res.write('</html>');
+//   res.end();
+};
+
+// module.exports = requestHandler;
+
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded text'
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some text';
+
+exports.handler = requestHandler;
+exports.someText = 'Some hard coded text';
