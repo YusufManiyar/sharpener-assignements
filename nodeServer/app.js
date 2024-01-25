3c4e1169b0ec5490829f52ed9f480e10cadd7f24
Tejas150 <tejaskamble1629@gmail.com>
2024-01-25 21:35:16 +0530

    add product route with product name and size

--- nodeServer/app.js
+++ nodeServer/app.js
@@ -2,18 +2,33 @@
 const http = require('http');
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log('In this middleware')
-    next()
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type = "text" name = "size"><button type="submit">Add Product</button></form>')
 })
 
-app.use((req, res, next) => {
-    console.log('In this another middleware')
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+
+// app.use((req, res, next) => {
+//     console.log('In this middleware')
+//     next()
+// })
+
+app.use('/',(req, res, next) => {
+    // console.log('In this another middleware')
     res.send('<h1> Hello from Express</h1>')
-    next()
+    // next()
 })
 
 const server = http.createServer(app);
